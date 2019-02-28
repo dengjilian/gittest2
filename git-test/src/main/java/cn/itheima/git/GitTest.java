f10276a1d159bcc185205360690876c38338f130
dengjilian <657473222@qq.com>
2019-02-28 20:22:01 +0800

    提交项目到本地仓库

--- git-test/src/main/java/cn/itheima/git/GitTest.java
+++ git-test/src/main/java/cn/itheima/git/GitTest.java
@@ -3,5 +3,6 @@ package cn.itheima.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("修改后提交到本地版本库");
     }
 }
